7204441116d31addcb3da01e975792af7469076c
HendrickFS <hendrickfel@gmail.com>
2022-04-29 01:26:31 -0300

    finalizando páginas

--- LightDark.js
+++ LightDark.js
@@ -1,4 +1,13 @@
 function DarkLight() {
+    c++;
     var element = document.body;
     element.classList.toggle("LightMode");
+    const txt = document.querySelector('button');
+    if (c%2==0){
+      txt.innerHTML='Light';
+    }else{
+      txt.innerHTML='Dark';
+    }
+    
   }
+  var c = 0;
